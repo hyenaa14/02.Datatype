c0fe70af581b2bb9b15fde99adbc7afde72322eb
hyenaa14 <hyenaa8257@naver.com>
2024-07-02 16:17:35 +0900

    hhh

---
+++ list.py
@@ -0,0 +1,19 @@
+hyena=["강혜나",'29','010-1234-1111']
+name = hyena[0]
+age = hyena[1]
+phone = hyena[2]
+print(type(hyena))
+[print(name, age, phone)]
+names = [['강수경','강혜나','김민석'],['20','21','22'],['010-****-****','010-****-****']]
+print(names[0][0:2])
+
+numbers = [1,2,3,4,5]
+result= numbers[2]+ numbers[-1]
+print(len(names[0])
+
+#리스트에 요소 추가하기
+last=[1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
---
+++ number.py
@@ -0,0 +1,14 @@
+string='1개 2개'
+int = 1
+
+print(string)
+print(int)
+
+print(type(int))
+
+
+number_1 = 10
+number_2 = 5
+
+print(number_1% number_2)
+
